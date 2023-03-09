876f0c742c3e7fc056be061edeb09631f11c9d41
Julian Abhari <julian.abhari20@gmail.com>
2023-03-09 13:23:37 -0600

    added keyboard input and jump mode????

--- Mod06a-Abhari/Assets/Scripts/PlayerController.cs
+++ Mod06a-Abhari/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,29 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float angleSpeed;
+    // Input handler
+    public float horizontalInput;
+    public float forwardInput;
+    public float jumpInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 15;
-        angleSpeed = 0;
+        speed = 20;
+        angleSpeed = 45;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Getting input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        jumpInput = Input.GetAxis("Jump");
+
         // Move the vehicle
-        transform.Translate(new Vector3(0, 0, speed) * Time.deltaTime);
-        transform.Translate(Vector3.right * Time.deltaTime * angleSpeed);
+        transform.Translate(new Vector3(0, 1, 0) * speed * jumpInput * Time.deltaTime);
+        transform.Translate(new Vector3(0, 0, 1) * speed * forwardInput * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 1, 0), angleSpeed * horizontalInput * Time.deltaTime);
     }
 }
